021596cd4947da452ac9945214ebd9b29693bfbe
devhassan <abdulwahabhassan645@gmail.com>
2022-01-12 10:38:18 +0100

    Initial Commit

---
+++ pinch-app/pinch_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_appApp.swift
+//  pinch-app
+//
+//  Created by Hassan Abdulwahab on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct pinch_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
